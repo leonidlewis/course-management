c7cf3ad0a3b26aff1ed10cd638982f6e8970f286
leonidlewis <leonid.lewis@gmail.com>
2023-07-18 09:17:44 +0900

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <x-slot name="header" class="d-flex">
+       <div class="col-3 p-5">
+    Thisis log
+    </div>
+    <div class="col-9">this is content</div> 
+    </x-slot>
+
+    <div class="py-12">
+        
+    </div>
+</x-app-layout>
